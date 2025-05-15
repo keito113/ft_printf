401805c0b86ead443b8e5fa2381db5c8da1d76c9
Keito Abe <keitabe@c4r3s14.42tokyo.jp>
2025-05-15 13:34:43 +0900

    Makefile_change

--- Makefile
+++ Makefile
@@ -6,48 +6,91 @@
 #    By: keitabe <keitabe@student.42tokyo.jp>       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/05/14 17:09:03 by keitabe           #+#    #+#              #
-#    Updated: 2025/05/14 18:31:43 by keitabe          ###   ########.fr        #
+#    Updated: 2025/05/15 12:20:08 by keitabe          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-NAME := libftprintf.a
-LIBFT_DIR := ../libft
-LIBFT := $(LIBFT_DIR)/libft.a
-CC := cc
-CFLAGS := -Wall -Wextra -Werror
-CFLAGS += -I. -I$(LIBFT_DIR)
-AR := ar
-ARFLAGS := rcs
+NAME       := libftprintf.a
+LIBFT_DIR  := libft
+LIBFT      := $(LIBFT_DIR)/libft.a
+
+CC         := cc
+CFLAGS     := -Wall -Wextra -Werror -I. -Ilibft
+AR         := ar
+ARFLAGS    := rcs
 
 SRCS =  ft_printf.c \
 		ft_spec_char.c \
 		ft_spec_hex.c \
 		ft_spec_num.c \
-		ft_spec_string_ptr.c \
-		main.c
-OBJS = $(SRCS:.c=.o)
+		ft_spec_string_ptr.c
+
+LIBFT_SRCS := \
+	libft/ft_atoi.c \
+	libft/ft_calloc.c \
+	libft/ft_isalnum.c \
+	libft/ft_isalpha.c \
+	libft/ft_isprint.c \
+	libft/ft_bzero.c \
+	libft/ft_itoa.c \
+	libft/ft_memchr.c \
+	libft/ft_memcmp.c \
+	libft/ft_memcpy.c \
+	libft/ft_memmove.c \
+	libft/ft_memset.c \
+	libft/ft_putchar_fd.c \
+	libft/ft_putendl_fd.c \
+	libft/ft_putnbr_fd.c \
+	libft/ft_putstr_fd.c \
+	libft/ft_split.c \
+	libft/ft_strchr.c \
+	libft/ft_strdup.c \
+	libft/ft_striteri.c \
+	libft/ft_strjoin.c \
+	libft/ft_strlcat.c \
+	libft/ft_strlcpy.c \
+	libft/ft_strlen.c \
+	libft/ft_strmapi.c \
+	libft/ft_strncmp.c \
+	libft/ft_strnstr.c \
+	libft/ft_strrchr.c \
+	libft/ft_strtrim.c \
+	libft/ft_substr.c \
+	libft/ft_toupper.c \
+	libft/ft_tolower.c \
+	libft/ft_isdigit.c \
+	libft/ft_isascii.c \
+	libft/ft_lstnew_bonus.c \
+	libft/ft_lstadd_front_bonus.c \
+	libft/ft_lstsize_bonus.c \
+	libft/ft_lstlast_bonus.c \
+	libft/ft_lstadd_back_bonus.c \
+	libft/ft_lstdelone_bonus.c \
+	libft/ft_lstclear_bonus.c \
+	libft/ft_lstiter_bonus.c \
+	libft/ft_lstmap_bonus.c
+
+OBJS := $(SRCS:%.c=%.o)
+
+LIBFT_OBJ := $(LIBFT_SRCS:%.c=%.o)
 
 all: $(NAME)
 
-$(NAME):$(OBJS)
-	$(AR) $(ARFLAGS) $(NAME) $(OBJS)
+$(NAME):$(OBJS) $(LIBFT_OBJ)
+	$(AR) $(ARFLAGS) $(NAME) $(OBJS) $(LIBFT_OBJ)
 
-$(LIBFT):
-	$(MAKE) -C $(LIBFT_DIR)
+%.o: %.c ft_printf.h libft/libft.h
+	$(CC) $(CFLAGS) -c $< -o $@
 
-test: all
-	$(MAKE) -C $(LIBFT_DIR)
-	$(CC) $(CFLAGS) main.c $(NAME) $(LIBFT) -o test_printf
-
-%.o: %.c ft_printf.h
+libft/%.o: libft/%.c libft/libft.h
 	$(CC) $(CFLAGS) -c $< -o $@
 
 clean:
-	rm -f $(OBJS)
+	rm -f $(OBJS) $(LIBFT_OBJ)
 
 fclean: clean
-	rm -f $(NAME)
+	rm -f $(NAME) $(LIBFT_OBJ)
 
 re: fclean all
 
-.PHONY: all clean fclean re
+.PHONY: all clean fclean re $(LIBFT_DIR)
--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,13 @@
 /*   By: keitabe <keitabe@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 14:17:12 by keitabe           #+#    #+#             */
-/*   Updated: 2025/05/14 18:53:52 by keitabe          ###   ########.fr       */
+/*   Updated: 2025/05/15 10:59:15 by keitabe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static size_t	putchar_len(char c)
+static int	putchar_len(char c)
 {
 	write(1, &c, 1);
 	return (1);
@@ -72,167 +72,165 @@ int	ft_printf(const char *str, ...)
 	va_end(ap);
 	return (len);
 }
-#if 0
-# include "ft_printf.h"
-# include <limits.h>
-# include <stdio.h>
 
-int	main(void)
-{
-	int	var;
+// #include "ft_printf.h"
+// #include <limits.h>
+// #include <stdio.h>
 
-    int r1, r2;
-    var = 42;
-    printf("Test 1: %%c 'A'\n");
-    printf("orig: ");
-    r1 = printf("%c", 'A');
-    printf(" | ret=%d\n", r1);
-    printf("ft : ");
-    r2 = ft_printf("%c", 'A');
-    printf(" | ret=%d\n\n", r2);
-    printf("Test 2: %%c 256\n");
-    printf("orig: ");
-    r1 = printf("%c", 256);
-    printf(" | ret=%d\n", r1);
-    printf("ft : ");
-    r2 = ft_printf("%c", 256);
-    printf(" | ret=%d\n\n", r2);
-    printf("Test 3: %%%% literal\n");
-    printf("orig: ");
-    r1 = printf("%%");
-    printf(" | ret=%d\n", r1);
-    printf("ft : ");
-    r2 = ft_printf("%%");
-    printf(" | ret=%d\n\n", r2);
-    printf("Test 4: %%s \"Hello, world!\"\n");
-    printf("orig: ");
-    r1 = printf("%s", "Hello, world!");
-    printf(" | ret=%d\n", r1);
-    printf("ft : ");
-    r2 = ft_printf("%s", "Hello, world!");
-    printf(" | ret=%d\n\n", r2);
-    printf("Test 5: %%s empty string\n");
-    printf("orig: ");
-    r1 = printf("%s", "");
-    printf(" | ret=%d\n", r1);
-    printf("ft : ");
-    r2 = ft_printf("%s", "");
-    printf(" | ret=%d\n\n", r2);
-    printf("Test 6: %%s NULL\n");
-    printf("orig: ");
-    r1 = printf("%s", (char *)NULL);
-    printf(" | ret=%d\n", r1);
-    printf("ft : ");
-    r2 = ft_printf("%s", (char *)NULL);
-    printf(" | ret=%d\n\n", r2);
-    printf("Test 7: %%p &var\n");
-    printf("orig: ");
-    r1 = printf("%p", (void *)&var);
-    printf(" | ret=%d\n", r1);
-    printf("ft : ");
-    r2 = ft_printf("%p", (void *)&var);
-    printf(" | ret=%d\n\n", r2);
-    printf("Test 8: %%p NULL\n");
-    printf("orig: ");
-    r1 = printf("%p", (void *)0);
-    printf(" | ret=%d\n", r1);
-    printf("ft : ");
-    r2 = ft_printf("%p", (void *)0);
-    printf(" | ret=%d\n\n", r2);
-    printf("Test 9: %%d 0\n");
-    printf("orig: ");
-    r1 = printf("%d", 0);
-    printf(" | ret=%d\n", r1);
-    printf("ft : ");
-    r2 = ft_printf("%d", 0);
-    printf(" | ret=%d\n\n", r2);
-    printf("Test 10: %%i 42\n");
-    printf("orig: ");
-    r1 = printf("%i", 42);
-    printf(" | ret=%d\n", r1);
-    printf("ft : ");
-    r2 = ft_printf("%i", 42);
-    printf(" | ret=%d\n\n", r2);
-    printf("Test 11: %%d -42\n");
-    printf("orig: ");
-    r1 = printf("%d", -42);
-    printf(" | ret=%d\n", r1);
-    printf("ft : ");
-    r2 = ft_printf("%d", -42);
-    printf(" | ret=%d\n\n", r2);
-    printf("Test 12: %%d INT_MAX\n");
-    printf("orig: ");
-    r1 = printf("%d", INT_MAX);
-    printf(" | ret=%d\n", r1);
-    printf("ft : ");
-    r2 = ft_printf("%d", INT_MAX);
-    printf(" | ret=%d\n\n", r2);
-    printf("Test 13: %%i INT_MIN\n");
-    printf("orig: ");
-    r1 = printf("%i", INT_MIN);
-    printf(" | ret=%d\n", r1);
-    printf("ft : ");
-    r2 = ft_printf("%i", INT_MIN);
-    printf(" | ret=%d\n\n", r2);
-    printf("Test 14: %%u 0u\n");
-    printf("orig: ");
-    r1 = printf("%u", 0u);
-    printf(" | ret=%d\n", r1);
-    printf("ft : ");
-    r2 = ft_printf("%u", 0u);
-    printf(" | ret=%d\n\n", r2);
-    printf("Test 15: %%u 123456789u\n");
-    printf("orig: ");
-    r1 = printf("%u", 123456789u);
-    printf(" | ret=%d\n", r1);
-    printf("ft : ");
-    r2 = ft_printf("%u", 123456789u);
-    printf(" | ret=%d\n\n", r2);
-    printf("Test 16: %%u UINT_MAX\n");
-    printf("orig: ");
-    r1 = printf("%u", UINT_MAX);
-    printf(" | ret=%d\n", r1);
-    printf("ft : ");
-    r2 = ft_printf("%u", UINT_MAX);
-    printf(" | ret=%d\n\n", r2);
-    printf("Test 17: %%x 0u\n");
-    printf("orig: ");
-    r1 = printf("%x", 0u);
-    printf(" | ret=%d\n", r1);
-    printf("ft : ");
-    r2 = ft_printf("%x", 0u);
-    printf(" | ret=%d\n\n", r2);
-    printf("Test 18: %%x 48879u (0xBEEF)\n");
-    printf("orig: ");
-    r1 = printf("%x", 48879u);
-    printf(" | ret=%d\n", r1);
-    printf("ft : ");
-    r2 = ft_printf("%x", 48879u);
-    printf(" | ret=%d\n\n", r2);
-    printf("Test 19: %%X 48879u (0xBEEF)\n");
-    printf("orig: ");
-    r1 = printf("%X", 48879u);
-    printf(" | ret=%d\n", r1);
-    printf("ft : ");
-    r2 = ft_printf("%X", 48879u);
-    printf(" | ret=%d\n\n", r2);
-    printf("Test 20: %%x %%X UINT_MAX\n");
-    printf("orig: ");
-    r1 = printf("%x %X", UINT_MAX, UINT_MAX);
-    printf(" | ret=%d\n", r1);
-    printf("ft : ");
-    r2 = ft_printf("%x %X", UINT_MAX, UINT_MAX);
-    printf(" | ret=%d\n\n", r2);
-    printf("Test 21: Mix all specifiers\n");
-    printf("orig: ");
-    r1 = printf("Mix:%c%s%p%d%u%x%X%%", 'A', "Hi", (void *)&var, -1, 1u, 255u,
-		255u);
-    printf(" | ret=%d\n", r1);
-    printf("ft : ");
-    r2 = ft_printf("Mix:%c%s%p%d%u%x%X%%", 'A', "Hi", (void *)&var, -1, 1u,
-		255u, 255u);
-    printf(" | ret=%d\n", r2);
-    return (0);
-}
+// int	main(void)
+// {
+// 	int	var;
 
-#endif
+// 	int r1, r2;
+// 	var = 42;
+// 	printf("Test 1: %%c 'A'\n");
+// 	printf("orig: ");
+// 	r1 = printf("%c", 'A');
+// 	printf(" | ret=%d\n", r1);
+// 	ft_printf("ft :  ");
+// 	r2 = ft_printf("%c", 'A');
+// 	printf(" | ret=%d\n\n", r2);
+// 	printf("Test 2: %%c 256\n");
+// 	printf("orig: ");
+// 	r1 = printf("%c", 256);
+// 	printf(" | ret=%d\n", r1);
+// 	ft_printf("ft :  ");
+// 	r2 = ft_printf("%c", 256);
+// 	printf(" | ret=%d\n\n", r2);
+// 	printf("Test 3: %%%% literal\n");
+// 	printf("orig: ");
+// 	r1 = printf("%%");
+// 	printf(" | ret=%d\n", r1);
+// 	ft_printf("ft :  ");
+// 	r2 = ft_printf("%%");
+// 	printf(" | ret=%d\n\n", r2);
+// 	printf("Test 4: %%s \"Hello, world!\"\n");
+// 	printf("orig: ");
+// 	r1 = printf("%s", "Hello, world!");
+// 	printf(" | ret=%d\n", r1);
+// 	ft_printf("ft :  ");
+// 	r2 = ft_printf("%s", "Hello, world!");
+// 	printf(" | ret=%d\n\n", r2);
+// 	printf("Test 5: %%s empty string\n");
+// 	printf("orig: ");
+// 	r1 = printf("%s", "");
+// 	printf(" | ret=%d\n", r1);
+// 	ft_printf("ft :  ");
+// 	r2 = ft_printf("%s", "");
+// 	printf(" | ret=%d\n\n", r2);
+// 	printf("Test 6: %%s NULL\n");
+// 	printf("orig: ");
+// 	r1 = printf("%s", (char *)NULL);
+// 	printf(" | ret=%d\n", r1);
+// 	ft_printf("ft :  ");
+// 	r2 = ft_printf("%s", (char *)NULL);
+// 	printf(" | ret=%d\n\n", r2);
+// 	printf("Test 7: %%p &var\n");
+// 	printf("orig: ");
+// 	r1 = printf("%p", (void *)&var);
+// 	printf(" | ret=%d\n", r1);
+// 	ft_printf("ft :  ");
+// 	r2 = ft_printf("%p", (void *)&var);
+// 	printf(" | ret=%d\n\n", r2);
+// 	printf("Test 8: %%p NULL\n");
+// 	printf("orig: ");
+// 	r1 = printf("%p", (void *)0);
+// 	printf(" | ret=%d\n", r1);
+// 	ft_printf("ft :  ");
+// 	r2 = ft_printf("%p", (void *)0);
+// 	printf(" | ret=%d\n\n", r2);
+// 	printf("Test 9: %%d 0\n");
+// 	printf("orig: ");
+// 	r1 = printf("%d", 0);
+// 	printf(" | ret=%d\n", r1);
+// 	ft_printf("ft :  ");
+// 	r2 = ft_printf("%d", 0);
+// 	printf(" | ret=%d\n\n", r2);
+// 	printf("Test 10: %%i 42\n");
+// 	printf("orig: ");
+// 	r1 = printf("%i", 42);
+// 	printf(" | ret=%d\n", r1);
+// 	ft_printf("ft :  ");
+// 	r2 = ft_printf("%i", 42);
+// 	printf(" | ret=%d\n\n", r2);
+// 	printf("Test 11: %%d -42\n");
+// 	printf("orig: ");
+// 	r1 = printf("%d", -42);
+// 	printf(" | ret=%d\n", r1);
+// 	ft_printf("ft :  ");
+// 	r2 = ft_printf("%d", -42);
+// 	printf(" | ret=%d\n\n", r2);
+// 	printf("Test 12: %%d INT_MAX\n");
+// 	printf("orig: ");
+// 	r1 = printf("%d", INT_MAX);
+// 	printf(" | ret=%d\n", r1);
+// 	ft_printf("ft :  ");
+// 	r2 = ft_printf("%d", INT_MAX);
+// 	printf(" | ret=%d\n\n", r2);
+// 	printf("Test 13: %%i INT_MIN\n");
+// 	printf("orig: ");
+// 	r1 = printf("%i", INT_MIN);
+// 	printf(" | ret=%d\n", r1);
+// 	ft_printf("ft :  ");
+// 	r2 = ft_printf("%i", INT_MIN);
+// 	printf(" | ret=%d\n\n", r2);
+// 	printf("Test 14: %%u 0u\n");
+// 	printf("orig: ");
+// 	r1 = printf("%u", 0u);
+// 	printf(" | ret=%d\n", r1);
+// 	ft_printf("ft :  ");
+// 	r2 = ft_printf("%u", 0u);
+// 	printf(" | ret=%d\n\n", r2);
+// 	printf("Test 15: %%u 123456789u\n");
+// 	printf("orig: ");
+// 	r1 = printf("%u", 123456789u);
+// 	printf(" | ret=%d\n", r1);
+// 	ft_printf("ft :  ");
+// 	r2 = ft_printf("%u", 123456789u);
+// 	printf(" | ret=%d\n\n", r2);
+// 	printf("Test 16: %%u UINT_MAX\n");
+// 	printf("orig: ");
+// 	r1 = printf("%u", UINT_MAX);
+// 	printf(" | ret=%d\n", r1);
+// 	ft_printf("ft :  ");
+// 	r2 = ft_printf("%u", UINT_MAX);
+// 	printf(" | ret=%d\n\n", r2);
+// 	printf("Test 17: %%x 0u\n");
+// 	printf("orig: ");
+// 	r1 = printf("%x", 0u);
+// 	printf(" | ret=%d\n", r1);
+// 	ft_printf("ft :  ");
+// 	r2 = ft_printf("%x", 0u);
+// 	printf(" | ret=%d\n\n", r2);
+// 	printf("Test 18: %%x 48879u (0xBEEF)\n");
+// 	printf("orig: ");
+// 	r1 = printf("%x", 48879u);
+// 	printf(" | ret=%d\n", r1);
+// 	ft_printf("ft :  ");
+// 	r2 = ft_printf("%x", 48879u);
+// 	printf(" | ret=%d\n\n", r2);
+// 	printf("Test 19: %%X 48879u (0xBEEF)\n");
+// 	printf("orig: ");
+// 	r1 = printf("%X", 48879u);
+// 	printf(" | ret=%d\n", r1);
+// 	ft_printf("ft :  ");
+// 	r2 = ft_printf("%X", 48879u);
+// 	printf(" | ret=%d\n\n", r2);
+// 	printf("Test 20: %%x %%X UINT_MAX\n");
+// 	printf("orig: ");
+// 	r1 = printf("%x %X", UINT_MAX, UINT_MAX);
+// 	printf(" | ret=%d\n", r1);
+// 	ft_printf("ft :  ");
+// 	r2 = ft_printf("%x %X", UINT_MAX, UINT_MAX);
+// 	printf(" | ret=%d\n\n", r2);
+// 	printf("Test 21: Mix all specifiers\n");
+// 	printf("orig: ");
+// 	r1 = printf("Mix:%c%s%p%d%u%x%X%%", 'A', "Hi", (void *)&var, -1, 1u, 255u,
+// 			255u);
+// 	printf(" | ret=%d\n", r1);
+// 	ft_printf("ft :  ");
+// 	r2 = ft_printf("Mix:%c%s%p%d%u%x%X%%", 'A', "Hi", (void *)&var, -1, 1u,
+// 			255u, 255u);
+// 	printf(" | ret=%d\n", r2);
+// 	return (0);
+// }
